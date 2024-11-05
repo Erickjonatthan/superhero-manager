11826748f264b5b871d5fb58941ecd4980e12db1
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2024-11-05 18:04:08 -0300

    Initial commit

---
+++ src/main/java/com/erickprojetos/superhero_manager/SuperheroManagerApplication.java
@@ -0,0 +1,13 @@
+package com.erickprojetos.superhero_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroManagerApplication.class, args);
+	}
+
+}
